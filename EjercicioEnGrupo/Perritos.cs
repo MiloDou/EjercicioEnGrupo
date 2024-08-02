ec1c869ac4a469426a02945512a925a73ae2662a
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-08-02 17:01:57 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioEnGrupo/Perritos.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace EjercicioEnGrupo
+{
+    public class Perritos
+    {
+        public Perritos(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string nombre { get; set; }
+
+        public void MostrarPerro()
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("         MOSTAR PERROS");
+            Console.ResetColor();
+            Console.WriteLine("----------------------------------\n");
+            Console.WriteLine(nombre);
+        }
+
+        public Perritos AgregarPerre()
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("         AGREGAR PERRO ");
+            Console.ResetColor();
+            Console.WriteLine("----------------------------------\n");
+            Console.Write("Ingresa nombre del perre: ");
+            string perreName = Console.ReadLine();
+            Console.WriteLine("Perre agregade correctamente");
+            Perritos perre = new Perritos(perreName);
+            return perre;
+        }
+
+        public void DevolverPerro(List<Perritos> perritosList)
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("         DEVOLVER PERRO ");
+            Console.ResetColor();
+            Console.WriteLine("----------------------------------\n");
+            Perritos perritoEliminar = new Perritos(""); 
+            perritoEliminar = BuscarPerro(perritosList);
+            if (perritoEliminar == null) { return; }
+            perritosList.Remove(perritoEliminar);
+            Console.WriteLine("Enviamos a su perrito a una granja en el cielo correctamente");
+            Console.ReadLine();
+        }
+
+        public Perritos BuscarPerro(List<Perritos> perritosList)
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("         BUSCAR PERRO ");
+            Console.ResetColor();
+            Console.WriteLine("----------------------------------\n");
+            Console.Write("Ingresa el nombre del perro: ");
+            string perreName = Console.ReadLine();
+            foreach (Perritos p in perritosList)
+            {
+                if (perreName == p.nombre)
+                {
+                    Console.WriteLine("Se encontro su perro: " + perreName);
+                    return p;
+                }
+            }
+            Console.WriteLine("Ese perro no existe");
+            Console.ReadLine();
+
+            return null;
+        }
+
+        public void CambiarNombre(List<Perritos> perritosList)
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("         CAMBIAR NOMBRE ");
+            Console.ResetColor();
+            Console.WriteLine("----------------------------------\n");
+            Perritos perritoModificar = BuscarPerro(perritosList);
+            if (perritoModificar == null) { return; }
+            Console.WriteLine("Ingrese el nuevo nombre: ");
+            perritoModificar.nombre = Console.ReadLine();
+            Console.WriteLine("Nombre cambiado correctamente");
+        }
+    }
+}
---
+++ EjercicioEnGrupo/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using EjercicioEnGrupo;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Perritos> PerresList = new List<Perritos>();
+            Perritos newPerro = new Perritos("");
+
+            while (true)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("----------------------------------");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("         HOTEL DE PERROS ");
+                    Console.ResetColor();
+                    Console.WriteLine("----------------------------------\n");
+                    Console.WriteLine("1. Agregar Perros");
+                    Console.WriteLine("2. Mostrar Perros");
+                    Console.WriteLine("3. Buscar Perros");
+                    Console.WriteLine("4. Eliminar Perros");
+                    Console.WriteLine("5. Cambiar Nombre");
+                    Console.WriteLine("6. Salir");
+                    Console.Write("Choose an option: ");
+                    int option1 = Convert.ToInt32(Console.ReadLine());
+
+                    switch (option1)
+                    {
+                        case 1:
+                            {
+                                newPerro = newPerro.AgregarPerre();
+                                PerresList.Add(newPerro);
+                                break;
+                            }
+                        case 2:
+                            {
+                                foreach (var perrito in PerresList)
+                                {
+                                    perrito.MostrarPerro();
+                                }
+                                break;
+                            }
+                        case 3:
+                            {
+                                newPerro.BuscarPerro(PerresList);
+                                break;
+                            }
+                        case 4:
+                            {
+                                newPerro.DevolverPerro(PerresList);
+                                break;
+                            }
+                        case 5:
+                            {
+                                newPerro.CambiarNombre(PerresList);
+                                break;
+                            }
+                        case 6:
+                            {
+                                Console.WriteLine("Ciao User...");
+                                return;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Opción no válida. Intente de nuevo.");
+                                break;
+                            }
+                    }Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Se produjo un error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
